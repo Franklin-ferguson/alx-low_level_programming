f73540cd8a73686e2591f4fdb05f0bf55ece2c59
Franklin-ferguson <hayiborfranklin@gmail.com>
2023-10-14 06:15:17 -0700

    ./2-print_alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,15 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the alphabets  in lower case followed by a new line
+ * Description: This program prints the alphabets
+ *in lower case followed by a new line
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char character = 'a';
+
 	while (character <= 'z')
 	{
 		putchar(character);
