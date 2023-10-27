5834e24c480462737d541b99b0ab0d0710b313df
Franklin-ferguson <hayiborfranklin@gmail.com>
2023-10-27 11:51:43 -0700

    hi

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - Calculate x raised to the power of y.
+ * @x: the base number
+ * @y: the exponential number
+ * Return: The result of x raised to the power of y, or -1 if y is negative.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
