39efdbbc5aa72cd4527b3c1dec144fbfb607704a
Franklin-ferguson <hayiborfranklin@gmail.com>
2023-12-20 09:37:33 -0800

    hi

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - prints a file to the standard output
+ * @filename: is the name of the file to be openned
+ * @letter: is the length of the string
+ * Return: number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileD;
+	ssize_t lenR, lenW;
+	char *buffer;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fileD = open(filename, O_RDONLY);
+	if (buffer == NULL)
+	{
+		close(fileD);
+		return (0);
+	}
+	lenR = read(fileD, buffer, letters);
+	close(fileD);
+	if (lenR == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	lenW = write(STDOUT_FILENO, buffer, lenR);
+	free(buffer);
+	if (lenR != lenW)
+	{
+		return (0);
+	}
+	return (lenW);
+}
